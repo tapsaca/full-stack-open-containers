25e8c238bd44907803d0c88088b5fcf7ea2af314
tapsaca <98756983+tapsaca@users.noreply.github.com>
2024-03-20 23:40:01 +0200

    Add solution to exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = await Todo.findByIdAndUpdate(req.todo.id, req.body, { new: true })
+  console.log(todo)
+  res.send(todo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
